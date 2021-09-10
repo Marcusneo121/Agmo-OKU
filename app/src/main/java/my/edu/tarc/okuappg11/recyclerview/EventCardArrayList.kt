9be90557ec161f575ca6c14d20dd4f6765ea499e
teoh <teohk-pm18@student.tarc.edu.my>
2021-09-10 16:09:29 +0800

    kj first push (recyclerview half)

---
+++ app/src/main/java/my/edu/tarc/okuappg11/recyclerview/EventCardArrayList.kt
@@ -0,0 +1,5 @@
+package my.edu.tarc.okuappg11.recyclerview
+
+import android.net.Uri
+
+data class EventCardArrayList(var eventTitle: String ?= null, var eventDescription:String ?= null, var imageUri: Uri)
