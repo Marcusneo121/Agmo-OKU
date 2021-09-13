3d4226d38e81130fb01dbeac9188ee5c2023f71a
vanessa <vanessakoay129@gmail.com>
2021-09-13 15:37:01 +0800

    user profile amendments

--- app/src/main/java/my/edu/tarc/okuappg11/fragments/ProfileFragment.kt
+++ app/src/main/java/my/edu/tarc/okuappg11/fragments/ProfileFragment.kt
@@ -1,13 +1,16 @@
 package my.edu.tarc.okuappg11.fragments
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_profile.*
 import my.edu.tarc.okuappg11.activities.*
 import my.edu.tarc.okuappg11.databinding.FragmentProfileBinding
 
@@ -27,6 +30,8 @@ class ProfileFragment : Fragment() {
     private lateinit var bmAdapter: BookmarkAdapter
     private var userID: String? = null
     private var eventID: String? = null
+    private var userName: String? = null
+    private var userEmail: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,23 +40,6 @@ class ProfileFragment : Fragment() {
 
     }
 
-    /*private fun checkUserType(){
-        if (user == "Normal User"){
-            btnMyPostedEvents.visibility = View.VISIBLE
-            btnMyVolunteers.visibility = View.VISIBLE
-            btnMyEventHistory.visibility = View.INVISIBLE
-            btnAllUpcomingEvents.visibility = View.INVISIBLE
-
-            binding.tvUserType.text = "Normal User"
-        }else{
-            btnMyPostedEvents.visibility = View.INVISIBLE
-            btnMyVolunteers.visibility = View.INVISIBLE
-            btnMyEventHistory.visibility = View.VISIBLE
-            btnAllUpcomingEvents.visibility = View.VISIBLE
-
-            binding.tvUserType.text = "OKU"
-        }
-    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,29 +51,39 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        // On think is that go to the Design there put the Username, Email and Role Type's text all put blank
-        // Cuz it looks weird at the first glance that user will see the default text you set.
-        // So just make the text blank and let you coding to assign the all the text according to all the user details
-
         fStore = FirebaseFirestore.getInstance()
         fAuth = FirebaseAuth.getInstance()
         userID = fAuth.currentUser?.uid
         fStore.collection("users").document(userID!!).get().addOnSuccessListener { it ->
             userRole = it.get("userType").toString()
             if(userRole == "Normal" ){
+                btnMyPostedEvents.visibility = View.VISIBLE
+                btnMyVolunteers.visibility = View.VISIBLE
+                btnMyEventHistory.visibility = View.INVISIBLE
+                btnAllUpcomingEvents.visibility = View.INVISIBLE
+                userName = it.get("name").toString()
+                userEmail = it.get("email").toString()
                 binding.tvUserType.text = "Normal User"
-                // at here enable those button for Normal User, and disable those for OKU
+                binding.tvUserName.text = "$userName"
+                binding.tvUserEmail.text = "$userEmail"
+
 
             } else {
+                btnMyPostedEvents.visibility = View.INVISIBLE
+                btnMyVolunteers.visibility = View.INVISIBLE
+                btnMyEventHistory.visibility = View.VISIBLE
+                btnAllUpcomingEvents.visibility = View.VISIBLE
+                userName = it.get("name").toString()
+                userEmail = it.get("email").toString()
                 binding.tvUserType.text = "OKU User"
-                // at here enable those button for OKU User, and disable those for Normal
+                binding.tvUserName.text = "$userName"
+                binding.tvUserEmail.text = "$userEmail"
 
             }
         }.addOnFailureListener {
-            //Here see u wanna put hamik, in case any failure
+            Log.w(ContentValues.TAG,"UNABLE TO ADD ${it.suppressedExceptions}")
         }
 
-        //checkUserType()
         binding.btnBookmarks.setOnClickListener {
             val intent = Intent(activity, Bookmark::class.java)
             activity?.startActivity(intent)
