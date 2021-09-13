418c2060039e3292d1cebe7f8546f278ee972597
Marcusneo121 <marcus121neo@gmail.com>
2021-09-13 14:13:15 +0800

    Profile Code A Bit

--- app/src/main/java/my/edu/tarc/okuappg11/fragments/ProfileFragment.kt
+++ app/src/main/java/my/edu/tarc/okuappg11/fragments/ProfileFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import my.edu.tarc.okuappg11.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var fStore: FirebaseFirestore
+    private lateinit var fAuth: FirebaseAuth
+    private var userID: String? = null
+    private var userRole: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +32,27 @@ class ProfileFragment : Fragment() {
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fStore = FirebaseFirestore.getInstance()
+        fAuth = FirebaseAuth.getInstance()
+        userID = fAuth.currentUser?.uid
+        fStore.collection("users").document(userID!!).get().addOnSuccessListener { it ->
+            userRole = it.get("userType").toString()
+            if(userRole == "Normal" ){
+
+
+            } else {
+
+
+            }
+        }.addOnFailureListener {
+
+        }
+
+
     }
 }
